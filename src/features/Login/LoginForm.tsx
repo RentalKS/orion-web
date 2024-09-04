8f4b211d353a8f770b9f3a1b9a101b60f64c0814
asalihaj <76753549+asalihaj@users.noreply.github.com>
2024-09-04 23:59:40 +0200

    Create basic login page component

---
+++ src/features/Login/LoginForm.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { useForm } from 'react-hook-form';
+import 'antd/dist/reset.css';
+
+interface LoginFormInputs {
+    username: string;
+    password: string;
+    remember: boolean;
+}
+
+const LoginForm: React.FC = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm<LoginFormInputs>();
+
+    const onSubmit = (data: LoginFormInputs) => {
+        console.log(data);
+    };
+
+    return (
+        <Form
+            name="login_form"
+            className="login-form"
+            initialValues={{ remember: true }}
+            onFinish={handleSubmit(onSubmit)}
+            style={{ maxWidth: '300px', margin: '0 auto', padding: '2rem 1rem', background: '#fff', borderRadius: '8px' }}
+        >
+            <Form.Item
+                validateStatus={errors.username ? 'error' : ''}
+                help={errors.username ? errors.username.message : ''}
+            >
+                <Input
+                    {...register('username')}
+                    prefix={<UserOutlined className="site-form-item-icon" />}
+                    placeholder="Username"
+                />
+            </Form.Item>
+
+            <Form.Item
+                validateStatus={errors.password ? 'error' : ''}
+                help={errors.password ? errors.password.message : ''}
+            >
+                <Input.Password
+                    {...register('password')}
+                    prefix={<LockOutlined className="site-form-item-icon" />}
+                    type="password"
+                    placeholder="Password"
+                />
+            </Form.Item>
+
+            <Form.Item>
+                <Checkbox {...register('remember')}>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item>
+                <Button type="primary" htmlType="submit" className="login-form-button" block>
+                    Log in
+                </Button>
+            </Form.Item>
+        </Form>
+    );
+};
+
+export default LoginForm;
