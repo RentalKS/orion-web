738cf30f5c3fcec64564a713f5f376f8d89af88d
asalihaj <76753549+asalihaj@users.noreply.github.com>
2024-09-05 00:04:30 +0200

    Setup react query provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app/App.tsx'
 import './index.css'
+import ReactQueryProvider from './providers/ReactQueryProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <ReactQueryProvider>
+        <App />
+    </ReactQueryProvider>
 )
