df9dd5d0457ffa53789f0364f57bbee2ac70f087
asalihaj <76753549+asalihaj@users.noreply.github.com>
2024-09-04 23:59:09 +0200

    Change App.tsx location and add react router dom dependency

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+
+function App() {
+    return (
+        <div style={{
+            height: '100vh',
+            width: '100vw'
+        }}>
+            <Navbar />
+        </div>
+    )
+}
+
+export default App
