0fbdeecd3ba1876d6fe00e25a3a379d0b2107b6a
asalihaj <76753549+asalihaj@users.noreply.github.com>
2024-09-06 23:35:32 +0200

    Temporary disable react query auth config

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,11 @@
+import Navbar from "../components/Navbar/navbar";
 import { AppProvider } from "./provider";
 import { AppRouter } from "./router";
 
 function App() {
     return (
         <AppProvider>
+            <Navbar />
             <AppRouter />
         </AppProvider>
     )
--- src/lib/auth.tsx
+++ src/lib/auth.tsx
@@ -1,35 +1,33 @@
-import { configureAuth } from 'react-query-auth';
 import { Navigate, useLocation } from 'react-router-dom';
 
-import apiClient from './api-client';
 
-const logout = (): Promise<void> => {
-    return apiClient.post('/auth/logout');
-};
+// const logout = (): Promise<void> => {
+//     return apiClient.post('/auth/logout');
+// };
 
-const authConfig = {
-    userFn: () => { },
-    loginFn: async () => { },
-    registerFn: async () => { },
-    logoutFn: logout,
-};
+// const authConfig = {
+//     userFn: () => { },
+//     loginFn: async () => { },
+//     registerFn: async () => { },
+//     logoutFn: logout,
+// };
 
 // eslint-disable-next-line react-refresh/only-export-components
-export const { useUser, useLogin, useLogout, useRegister, AuthLoader } =
-    configureAuth(authConfig);
+// export const { useUser, useLogin, useLogout, useRegister, AuthLoader } =
+//     configureAuth(authConfig);
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-    const user = useUser();
+    // const user = useUser();
     const location = useLocation();
 
-    if (!user.data) {
-        return (
-            <Navigate
-                to={`/auth/login?redirectTo=${encodeURIComponent(location.pathname)}`}
-                replace
-            />
-        );
-    }
+    // if (!user.data) {
+    return (
+        <Navigate
+            to={`/auth/login?redirectTo=${encodeURIComponent(location.pathname)}`}
+            replace
+        />
+    );
 
-    return children;
+    // return children;
 };
