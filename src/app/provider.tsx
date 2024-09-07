5fdb059b27bd4535827f1214a2fbfa78b6062e5b
asalihaj <76753549+asalihaj@users.noreply.github.com>
2024-09-08 00:23:36 +0200

    Add theme config values

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import * as React from 'react';
 
 import { queryConfig } from '../lib/react-query';
+import { ConfigProvider } from 'antd';
 
 type AppProviderProps = {
     children: React.ReactNode;
@@ -23,17 +24,30 @@ export const AppProvider = ({ children }: AppProviderProps) => {
                 </div>
             }
         >
-            <QueryClientProvider client={queryClient}>
-                {/* <AuthLoader
+            <ConfigProvider
+                theme={{
+                    token: {
+                        "colorPrimary": "#006aff",
+                        "colorInfo": "#006aff",
+                        "colorError": "#ff2727",
+                        "colorSuccess": "#53c940",
+                        "colorTextBase": "#191919",
+                        "colorBgBase": "#f8f7f1",
+                    }
+                }}
+            >
+                <QueryClientProvider client={queryClient}>
+                    {/* <AuthLoader
                     renderLoading={() => (
                         <div className="flex h-screen w-screen items-center justify-center">
-                             TODO: Add loading component
+                        TODO: Add loading component
                         </div>
-                    )}
-                > */}
-                {children}
-                {/* </AuthLoader> */}
-            </QueryClientProvider>
+                        )}
+                        > */}
+                    {children}
+                    {/* </AuthLoader> */}
+                </QueryClientProvider>
+            </ConfigProvider>
         </React.Suspense>
     );
 };
