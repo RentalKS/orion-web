e0067af92c5b7c31d94ff03b9519213786179f33
asalihaj <76753549+asalihaj@users.noreply.github.com>
2024-09-08 01:36:18 +0200

    Remove navbar and reset css styling

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,9 @@
-import Navbar from "../components/Navbar/navbar";
 import { AppProvider } from "./provider";
 import { AppRouter } from "./router";
 
 function App() {
     return (
         <AppProvider>
-            <Navbar />
             <AppRouter />
         </AppProvider>
     )
