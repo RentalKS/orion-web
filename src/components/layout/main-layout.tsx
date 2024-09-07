340b440f60f9ade7f2865370c0ee443194a03769
asalihaj <76753549+asalihaj@users.noreply.github.com>
2024-09-08 01:48:28 +0200

    Add css file

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -1,9 +1,12 @@
-import { Divider, Layout, Menu } from 'antd';
+import { Divider, Layout, Menu, Typography } from 'antd';
 
 import { ArrowRightLeft, Bell, CarFront, CreditCard, LayoutDashboard, Newspaper, NotebookText, Settings } from 'lucide-react';
 import Paragraph from 'antd/es/typography/Paragraph';
 
+import './styles.css';
+
 const { Sider, Content } = Layout;
+const { Title } = Typography;
 
 interface MainLayoutProps {
     contentChildren: React.ReactNode;
@@ -11,40 +14,40 @@ interface MainLayoutProps {
 
 function MainLayout({ contentChildren }: MainLayoutProps) {
     return (
-        <Layout style={{ minHeight: '100vh' }}>
-            <Sider width={250} theme="dark" style={{ paddingInline: '1.4rem', paddingTop: "4rem" }}>
-                <div className="logo" />
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-                    <Menu.Item key="1" icon={<LayoutDashboard size={20} />} style={{ paddingLeft: "18px" }}>
+        <Layout style={{ minHeight: "100vh" }}>
+            <Sider width={250} theme="dark" style={{ paddingInline: "1.4rem", paddingTop: "4rem" }}>
+                <Title style={{ color: "#fff", textAlign: "center", textTransform: "uppercase" }}>Orion</Title>
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+                    <Menu.Item key="1" icon={<LayoutDashboard size={20} />} className="menu-item">
                         Dashboard
                     </Menu.Item>
-                    <Menu.Item key="2" icon={<CarFront size={20} />} style={{ paddingLeft: "18px" }}>
+                    <Menu.Item key="2" icon={<CarFront size={20} />} className="menu-item">
                         Vehicles
                     </Menu.Item>
-                    <Menu.Item key="3" icon={<NotebookText size={20} />} style={{ paddingLeft: "18px" }}>
+                    <Menu.Item key="3" icon={<NotebookText size={20} />} className="menu-item">
                         Bookings
                     </Menu.Item>
-                    <Menu.Item key="4" icon={<Bell size={20} />} style={{ paddingLeft: "18px" }}>
+                    <Menu.Item key="4" icon={<Bell size={20} />} className="menu-item">
                         Notifications
                     </Menu.Item>
-                    <Menu.Item key="5" icon={<Settings size={20} />} style={{ paddingLeft: "18px" }}>
+                    <Menu.Item key="5" icon={<Settings size={20} />} className="menu-item">
                         Settings
                     </Menu.Item>
-                    <Divider />
+                    <Divider style={{ backgroundColor: "#6c757d" }} />
                     <Paragraph style={{ color: "#fff", marginLeft: "1rem" }}>Report</Paragraph>
-                    <Menu.Item key="6" icon={<CreditCard size={20} />} style={{ paddingLeft: "18px" }}>
+                    <Menu.Item key="6" icon={<CreditCard size={20} />} className="menu-item">
                         Payment Details
                     </Menu.Item>
-                    <Menu.Item key="7" icon={<ArrowRightLeft size={20} />} style={{ paddingLeft: "18px" }}>
+                    <Menu.Item key="7" icon={<ArrowRightLeft size={20} />} className="menu-item">
                         Transactions
                     </Menu.Item>
-                    <Menu.Item key="8" icon={<Newspaper size={20} />} style={{ paddingLeft: "18px" }}>
+                    <Menu.Item key="8" icon={<Newspaper size={20} />} className="menu-item">
                         Car Report
                     </Menu.Item>
                 </Menu>
             </Sider>
             <Layout>
-                <Content style={{ margin: '16px', backgroundColor: '#00000010' }}>
+                <Content style={{ margin: "16px", backgroundColor: "#00000010" }}>
                     {contentChildren}
                 </Content>
             </Layout>
